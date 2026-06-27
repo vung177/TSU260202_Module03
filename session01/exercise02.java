21f83f85f3920927ef96b66f592464e2b95b4953
vung177 <vu.ng177@gmail.com>
2026-06-27 18:54:46 +0900

    updated

--- session01/exercise02.java
+++ session01/exercise02.java
@@ -4,7 +4,7 @@ public class exercise02 {
     public static void main(String[] args) {
         // Bước 1: Khai báo các biến lưu trữ: tên (String), tuổi (int), địa chỉ (String)
         String name = "Nguyễn Văn A";
-        // Bước 2: Dùng System.out.printf() để in ra màn hình theo định dạng đề bài yêu
+        // Bước 2: In ra màn hình theo định dạng đề bài yêu cầu
         int age = 20;
         String address = "Hồ Chí Minh";
         System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s\n", name, age, address);
