374b5322fa67498328ae50d92159f4acc8534b66
vung177 <vu.ng177@gmail.com>
2026-06-27 19:34:41 +0900

    updated

---
+++ session01/exercise03.java
@@ -0,0 +1,18 @@
+package Session01;
+
+import java.util.Scanner;
+
+public class exercise03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Nhập vào bán kính: ");
+        double radius = scanner.nextDouble();
+        double area = Math.PI * radius * radius;
+        if (radius == 0) {
+            System.out.println("Diện tích : 0");
+        } else {
+            System.out.printf("Diện tích : %.2f\n", area);
+        }
+        scanner.close();
+    }
+}
