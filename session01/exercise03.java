0c44b814be292323a377feeb10a4dc3f7a7eaf53
vung177 <vu.ng177@gmail.com>
2026-06-27 20:10:22 +0900

    updaed

--- session01/exercise03.java
+++ session01/exercise03.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 
 public class exercise03 {
     public static void main(String[] args) {
+        // Tạo đối tượng Scanner để nhập dữ liệu người dùng
         Scanner scanner = new Scanner(System.in);
+
+        // Yêu cầu người dùng nhập bán kính
         System.out.printf("Nhập bán kính của hình tròn: ");
+
+        // Khai báo biến radius và area
         double radius = scanner.nextDouble();
         double area = Math.PI * radius * radius;
+
+        // Hiển thị diện tích hình tròn
         if (radius == 0) {
             System.out.println("Diện tích của hình tròn là: 0");
         } else {
