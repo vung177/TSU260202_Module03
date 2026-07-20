260b0a551f51eaee361ce2658d8ff534c913a90c
vung177 <vu.ng177@gmail.com>
2026-07-20 10:04:41 +0900

    updated

--- Session04/exercise03.java
+++ Session04/exercise03.java
@@ -1,8 +1,41 @@
 package Session04;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class exercise03 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập kích thước mảng: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Nhập các phần tử cho mảng:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Sắp xếp mảng bằng thuật toán nổi bọt (Bubble Sort) theo thứ tự giảm dần
+        bubbleSort(arr);
+
+        // In kết quả
+        System.out.println("Mảng sau khi sắp xếp theo thứ tự giảm dần: " + Arrays.toString(arr));
+
+        scanner.close();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    // Tráo đổi phần tử
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
